ed71c7b897052d7646043663e9ea24d66205baf7
Александр <111alexandr2010@mail.ru>
2019-06-03 11:31:41 +0700

    the creating of method "chanceToExit"

--- Temperature/src/ru/academits/ikonnikov/temperature/Temperature.java
+++ Temperature/src/ru/academits/ikonnikov/temperature/Temperature.java
@@ -1,25 +1,21 @@
 package ru.academits.ikonnikov.temperature;
 
+import ru.academits.ikonnikov.temperature.gui.View;
+
 import javax.swing.*;
 
 public class Temperature {
-    /*private static boolean isNumeric(String stringNumber) {
-        try {
-            Double.parseDouble(stringNumber);
-            return true;
-        } catch ( NumberFormatException e ) {
-            return false;
-        }
-    }*/
-
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(
-                    UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch ( Exception e ) {
             e.printStackTrace();
         }
-        SwingUtilities.invokeLater(
-                Model::run);
+
+        SwingUtilities.invokeLater(() -> {
+            View view = new View();
+            view.run();
+            view.setVisible(true);
+        });
     }
 }
