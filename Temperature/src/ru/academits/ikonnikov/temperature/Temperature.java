1c10186a0169945761fdb454203b3ec0516524bb
Александр <111alexandr2010@mail.ru>
2019-06-30 14:18:04 +0700

    the correcting of cod by using of interface

--- Temperature/src/ru/academits/ikonnikov/temperature/Temperature.java
+++ Temperature/src/ru/academits/ikonnikov/temperature/Temperature.java
@@ -5,10 +5,8 @@ import ru.academits.ikonnikov.temperature.gui.View;
 
 public class Temperature {
     public static void main(String[] args) {
-        Scales[] scales = new Scales[3];
-        scales[0] = new ScaleCelsius();
-        scales[1] = new ScaleFahrenheit();
-        scales[2] = new ScaleKelvin();
+        Scales[] scales = new Scales[]{new ScaleCelsius(), new ScaleFahrenheit(), new ScaleKelvin()};
+
         View view = new View(scales);
         view.run();
         view.setVisible(true);
